671fba4a06b612eccf825ef7360f80670ed21a8d
kim_vu <60919541+Anna22042001@users.noreply.github.com>
2024-08-08 14:09:13 +0000

    nlvr2_added

---
+++ scripts/v1_5/eval/nlvr2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
+CHUNKS=${#GPULIST[@]}
+
+CKPT="llava-v1.5-7b"
+SPLIT="llava_nlvr2_questions"
+NLVR2DIR="./playground/data/eval/nlvr2/data"
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
+        --model-path liuhaotian/llava-v1.5-7b \
+        --question-file ./playground/data/eval/nlvr2/$SPLIT.jsonl \
+        --image-folder ./playground/data/eval/nlvr2/data/images \
+        --answers-file ./playground/data/eval/nlvr2/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl \
+        --num-chunks $CHUNKS \
+        --chunk-idx $IDX \
+        --temperature 0 \
+        --conv-mode vicuna_v1 &
+done
+
+wait
+
+output_file=./playground/data/eval/nlvr2/answers/$SPLIT/$CKPT/merge.jsonl
+
+# Clear out the output file if it exists.
+> "$output_file"
+
+# Loop through the indices and concatenate each file.
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat ./playground/data/eval/nlvr2/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
+
+python scripts/convert_nlvr2_for_eval.py --src $output_file --dst $NLVR2DIR/testdev_balanced_predictions.json
